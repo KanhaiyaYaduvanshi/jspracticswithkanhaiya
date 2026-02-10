4ac47c30ea294dd11f83c047b62c28ada0e0f3dc
Kanhaiya Yaduvanshi <kanhaiyakumar62310@gmail.com>
2026-02-10 13:24:51 +0000

    variables

---
+++ 01_Basics/variables.js
@@ -0,0 +1,14 @@
+
+const accountId = 14455
+let accountEmail = "khjaghhsnk@google.com"
+var accountPassword = "123345"
+accountCity = "Madhubani"
+
+//accountId = 2 not allowed
+accountEmail = "kanhaiya@google.com"
+accountPassword = "5454646"
+accountCity = "Mithila"
+
+console.log(accountId);
+console.table([accountId , accountEmail, accountPassword,accountCity])
+
