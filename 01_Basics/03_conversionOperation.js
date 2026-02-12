a9b3b3262ca79daee15a02f875b7701ed41f2cd7
Kanhaiya Yaduvanshi <kanhaiyakumar62310@gmail.com>
2026-02-12 13:30:48 +0000

    conversion operation done

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,29 @@
+let score = "kanhaiya"
+
+ console.log(typeof score);
+ console.log(typeof(score));
+
+ let valueInNumber = Number(score)
+ console.log(typeof valueInNumber);
+ console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false=>0
+
+ let boolean = 1
+ let booleanNum = Boolean(boolean)
+console.log(typeof booleanNum);
+console.log(booleanNum);
+
+
+
+// 1=> true; 0=> false
+// "" => false
+// "Kanhaiya" => true
+
+ let someNumber = 76
+let stringNumber = String(someNumber)
+ console.log(typeof stringNumber)
+ console.log(stringNumber);
+
