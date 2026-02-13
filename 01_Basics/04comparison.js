cd972d99dcb081f168c3e55517e6f8092a3f5f15
Kanhaiya Yaduvanshi <kanhaiyakumar62310@gmail.com>
2026-02-13 12:55:26 +0000

    datatype comparison

---
+++ 01_Basics/04comparison.js
@@ -0,0 +1,23 @@
+console.log( 2 > 1);
+console.log( 2 >= 1);
+console.log( 2 <= 1);
+console.log( 2 < 1);
+console.log( 2 == 1);
+console.log( 2 != 1);
+
+console.log("kanhaiya" > 2);
+console.log("2" > 1);
+
+
+console.log(null > 0);
+console.log(null >= 0);
+console.log(null == 0);
+
+console.log(undefined > 0);
+console.log(undefined >= 0);
+console.log(undefined == 0);
+//these type of comparison not gives predictable
+//  result that's why we have to avoid it in js
+
+//=== strict comparison check data type also
+console.log("2" === 2);
