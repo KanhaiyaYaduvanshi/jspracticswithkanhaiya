43e885992eac9d865e2cee45f8cb1440f442773d
Kanhaiya Yaduvanshi <kanhaiyakumar62310@gmail.com>
2026-02-16 12:01:33 +0000

    math & number

---
+++ 01_Basics/number&math.js
@@ -0,0 +1,29 @@
+const score = 400
+console.log(score);
+
+
+const balance = new Number(100.7757)
+console.log(balance);
+
+console.log(balance.toFixed(1));
+
+const otherNumber = 123.8966
+console.log(otherNumber.toPrecision(4));
+
+const hundreds = 1000000
+console.log(hundreds.toLocaleString("en-IN"));
+
+
+//*************math***************//
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.6));
+console.log(Math.floor(Math.random()*10) + 1);//random value more than 1
+
+const min = 10
+const max = 20
+
+console.log
+(Math.floor(Math.random() *(max-min + 1) )+min); //value more than min+
+
+
